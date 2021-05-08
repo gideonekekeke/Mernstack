13154d12d017e8c754b8c90c17810419e460d646
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-05-08 15:44:32 +0100

    mongodb and react api

---
+++ clientproject/src/data/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const apiUrl =
+  "mongodb+srv://shotkode:shotkode@cluster0.2kfdg.mongodb.net/shotkodeDB?retryWrites=true&w=majority";
+
+export const singleFileUpload = async (data, options) => {
+  try {
+    await axios.post(apiUrl + "singleFile", data, options);
+  } catch (error) {
+    throw error;
+  }
+};
+export const getSingleFiles = async () => {
+  try {
+    const { data } = await axios.get(apiUrl + "getSingleFiles");
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const multipleFilesUpload = async (data, options) => {
+  try {
+    await axios.post(apiUrl + "multipleFiles", data, options);
+  } catch (error) {
+    throw error;
+  }
+};
+export const getMultipleFiles = async () => {
+  try {
+    const { data } = await axios.get(apiUrl + "getMultipleFiles");
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
